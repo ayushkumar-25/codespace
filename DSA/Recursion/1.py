94fc4b60da1c1983a4f305c7cc3c873afdc3615a
Ayush Kumar <ayush2608@gmail.com>
2022-06-12 07:05:43 +0000

    ginors

--- DSA/Recursion/1.py
+++ DSA/Recursion/1.py
@@ -1,9 +1,15 @@
-def openRussianDoll(doll):
-    if doll == 1:
-        print("All dolls are opened")
-    else:
-        print(doll)
-        openRussianDoll(doll - 1)
+# def openRussianDoll(doll):
+#     if doll == 1:
+#         print("All dolls are opened")
+#     else:
+#         print(doll)
+#         openRussianDoll(doll - 1)
         
 
-openRussianDoll(10)
+# openRussianDoll(10)
+
+#print(~-5)
+
+order = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1357902468'
+#order = "ayush"
+print(*sorted(input("Enter : "), key=order.index), sep='')
